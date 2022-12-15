d2f19a70dcae63472de06f0500a30a25b29e0adb
diakt <igs2107@columbia.edu>
2022-12-15 13:25:01 -0800

    reintroduced django_on_heroku

--- admin_app/settings.py
+++ admin_app/settings.py
@@ -168,8 +168,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Configure Django App for Heroku.
-# import django_on_heroku
-# django_on_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
