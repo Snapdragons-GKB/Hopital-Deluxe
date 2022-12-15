b9a5a438de4bf8ed6955adf4d4974c1a9127db0e
diakt <igs2107@columbia.edu>
2022-12-15 10:50:42 -0800

    postslash

--- admin_app/settings.py
+++ admin_app/settings.py
@@ -163,7 +163,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
 
 
